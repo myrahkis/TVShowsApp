8ad97295b1de17425566b2125f9d0bed8225a283
Mira <kruzinka@yandex.ru>
2022-05-05 21:32:09 +0300

    test code in Controller class + new scene added

---
+++ project/src/main/java/com/example/project/Controller.java
@@ -0,0 +1,37 @@
+package com.example.project;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField loginInput;
+
+    @FXML
+    private Button okayButton;
+
+    @FXML
+    private PasswordField passwordInput;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    void initialize() {
+        okayButton.setOnAction(actionEvent -> {
+            System.out.println("Вы нажали на кнопку");
+        });
+    }
+
+}
