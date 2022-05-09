8c2e4262c80d95ddc63713b7cc998abc4f5c0adb
Mira <kruzinka@yandex.ru>
2022-05-09 18:19:23 +0300

    added switching between ExampleOfView and helloView

--- project/src/main/java/com/example/project/ExampleOfViewController.java
+++ project/src/main/java/com/example/project/ExampleOfViewController.java
@@ -1,11 +1,21 @@
 package com.example.project;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class ExampleOfViewController {
 
@@ -34,9 +44,25 @@ public class ExampleOfViewController {
     private TextField yearTextField;
 
     @FXML
-    void initialize() {
+    private Button backButton;
 
+//    @FXML
+//    void initialize() {
+//        backButton.setOnAction(actionEvent -> {
+//            System.out.println("Вы нажали на кнопку Назад");
+//        });
+//    }
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToHelloView(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- project/src/main/java/com/example/project/helloViewController.java
+++ project/src/main/java/com/example/project/helloViewController.java
@@ -1,11 +1,20 @@
 package com.example.project;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class helloViewController {
 
@@ -27,15 +36,26 @@ public class helloViewController {
     @FXML
     private Button signUpButton;
 
-    @FXML
-    void initialize() {
-        okayButton.setOnAction(actionEvent -> {
-            System.out.println("Вы нажали на кнопку Готово");
-        });
+//    @FXML
+//    void initialize() {
+//        okayButton.setOnAction(actionEvent -> {
+//            System.out.println("Вы нажали на кнопку Готово");
+//        });
+//
+//        signUpButton.setOnAction(actionEvent -> {
+//            System.out.println("Вы нажали на кнопку регистрации");
+//        });
+//    }
 
-        signUpButton.setOnAction(actionEvent -> {
-            System.out.println("Вы нажали на кнопку регистрации");
-        });
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToExampleOfView(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("ExampleOfView.fxml")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
-
 }
