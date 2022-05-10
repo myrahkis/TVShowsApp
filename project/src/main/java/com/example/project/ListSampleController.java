de5d6916e82cf40b0c51f43d7cad3939328c3f2d
Mira <kruzinka@yandex.ru>
2022-05-10 21:25:14 +0300

    major changes in ListSample
    changed the arrangement of elements, added new button and decided to use TabPane

--- project/src/main/java/com/example/project/ListSampleController.java
+++ project/src/main/java/com/example/project/ListSampleController.java
@@ -16,6 +16,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -28,49 +29,118 @@ public class ListSampleController {
     private URL location;
 
     @FXML
-    private Button addButton;
+    private Button addButtonStoppedWatching;
 
     @FXML
-    private Button deleteButton;
+    private Button addButtonWatched;
+
+    @FXML
+    private Button addButtonWatching;
+
+    @FXML
+    private Button deleteButtonStoppedWatching;
+
+    @FXML
+    private Button deleteButtonWatched;
+
+    @FXML
+    private Button deleteButtonWatching;
+
+    @FXML
+    private Button clearStoppedWatching;
+
+    @FXML
+    private Button clearWatched;
+
+    @FXML
+    private Button clearWatching;
 
     @FXML
     private Label list;
 
     @FXML
-    private Button stopWatchingButton;
-
-    @FXML
-    private Button watchedButton;
-
-    @FXML
-    private Button watchingButton;
-
-    @FXML
     private Button exitButton;
 
     @FXML
-    private ListView<String> listOfTVShows;
+    private ListView<String> listOfStoppedWatching;
 
-    String[] name = {"Breaking bad", "House, M.D.", "Stranger Things"};
+    @FXML
+    private ListView<String> listOfWatched;
 
-    String currentTVShow;
+    @FXML
+    private ListView<String> listOfWatching;
+
+    @FXML
+    private TextField name;
+
+    @FXML
+    void addNameToWatching(MouseEvent event) {
+        listOfWatching.getItems().add(name.getText());
+    }
+
+    @FXML
+    void deleteNameFromWatching(MouseEvent event) {
+        int selectedTVShow = listOfWatching.getSelectionModel().getSelectedIndex();
+        listOfWatching.getItems().remove(selectedTVShow);
+    }
+
+    @FXML
+    void addNameToWatched(MouseEvent event) {
+        listOfWatched.getItems().add(name.getText());
+    }
+
+    @FXML
+    void deleteNameFromWatched(MouseEvent event) {
+        int selectedTVShow = listOfWatched.getSelectionModel().getSelectedIndex();
+        listOfWatched.getItems().remove(selectedTVShow);
+    }
+
+    @FXML
+    void addNameToStoppedWatching(MouseEvent event) {
+        listOfStoppedWatching.getItems().add(name.getText());
+    }
+
+    @FXML
+    void deleteNameFromStoppedWatching(MouseEvent event) {
+        int selectedTVShow = listOfStoppedWatching.getSelectionModel().getSelectedIndex();
+        listOfStoppedWatching.getItems().remove(selectedTVShow);
+    }
+
+    @FXML
+    void clearWatching(MouseEvent event) {
+        listOfWatching.getItems().clear();
+    }
+
+    @FXML
+    void clearWatched(MouseEvent event) {
+        listOfWatched.getItems().clear();
+    }
+
+    @FXML
+    void clearStoppedWatching(MouseEvent event) {
+        listOfStoppedWatching.getItems().clear();
+    }
+
+//    String[] name = {"Breaking bad", "House, M.D.", "Stranger Things"};
+//
+//    String currentTVShow;
 
 //    public void initialize() {
 //        listOfTVShows.getItems().addAll(name);
 //    }
 
-    public void initialize() {
-        listOfTVShows.getItems().addAll(name);
-
-        listOfTVShows.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                currentTVShow = listOfTVShows.getSelectionModel().getSelectedItem();
-
+//    public void initialize() {
+//        listOfTVShows.getItems().addAll(name);
+//
+//        listOfTVShows.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+//            @Override
+//            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+//                currentTVShow = listOfTVShows.getSelectionModel().getSelectedItem();
+//
 //                System.out.println(currentTVShow);
-            }
-        });
-    }
+//            }
+//        });
+//    }
 
 //    @FXML
 //    void initialize() {
@@ -100,14 +170,6 @@ public class ListSampleController {
     private Parent root;
 
     public void switchToHelloView(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchToExampleOfView(MouseEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hello-view.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
