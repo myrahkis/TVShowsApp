f20f9e299dbf5469478ba2f89428152c56c65102
Mira <kruzinka@yandex.ru>
2022-05-10 21:52:27 +0300

    skeleton added to the controller

--- project/src/main/java/com/example/project/RegistrationSceneController.java
+++ project/src/main/java/com/example/project/RegistrationSceneController.java
@@ -1,5 +1,41 @@
 package com.example.project;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class RegistrationSceneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button okaySignUpButton;
+
+    @FXML
+    private PasswordField signUpEmail;
+
+    @FXML
+    private PasswordField signUpLastName;
+
+    @FXML
+    private TextField signUpName;
+
+    @FXML
+    private ChoiceBox<?> signUpSexChoice;
+
+
+
+//    @FXML
+//    void initialize() {
+//
+//    }
 
 }
